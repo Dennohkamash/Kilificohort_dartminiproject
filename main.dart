537c7861560d06d21bfc4c0b29e5eb95689f2497
Dennohkamash <dennohkamash134@gmail.com>
2024-03-16 14:33:18 +0300

    update

--- main.dart
+++ main.dart
@@ -1,35 +1,37 @@
-
 import 'dart:io';
 
+//class student with its properties
 class Student {
   late String name;
   late int admn_no;
   late int cat_1;
   late int cat_2;
   late int main_exam;
-
+//constructor to initialize the property name and admnission number
   Student({
     required this.name,
     required this.admn_no,
   });
-
-  void printResults(int score) {
+//function that takes its mandatory parameters score and examtype to check the score and exam type
+  void printResults(int score, String examType) {
+    //is else statement to check if the score is within the stated condition
     if (score > 85) {
-      print("Excellent \u{1f603} You scored $score points");
+      print("Excellent \u{1f603} You scored $score points in $examType");
     } else if (score > 75) {
-      print("Very Good \u{1f603} You scored $score points");
+      print("Very Good \u{1f603} You scored $score points in $examType");
     } else if (score > 65) {
-      print("Good \u{1f603} You scored $score points");
+      print("Good \u{1f603} You scored $score points in $examType");
     } else {
-      print("Average \u{1f604} You scored $score points");
+      print("Average \u{1f604} You scored $score points in $examType");
     }
   }
 
+//function to evaluate results
   void evaluateResults() {
-    int total = cat_1 + cat_2 + main_exam;
-    double average = total / 3;
+    double average = (cat_1 + cat_2 + main_exam) / 3;
+    //rounding of the average to the nearest whole number
     int roundedAverage = average.round();
-
+//is else statement to check if the  average is within the stated condition
     if (roundedAverage > 85) {
       print("Excellent You scored an Average of $roundedAverage points");
     } else if (roundedAverage > 75) {
@@ -43,11 +45,21 @@ class Student {
 }
 
 void main() {
+  //getting users input
   stdout.write("Enter your name : ");
+  //read the input and store it variable name
   String name = stdin.readLineSync()!;
+  //getting users input
   stdout.write("Enter your Admn no : ");
+  //read the input convert into and interger and store it variable admn_no
   int admn_no = int.parse(stdin.readLineSync()!);
-  var average = Student(name: name, admn_no: admn_no);
+  //object to access the properties of the class student
+  var student = Student(name: name, admn_no: admn_no);
+  //boolean variables to check whether they contain values that we are going to use to store data
+  bool cat1Entered = false;
+  bool cat2Entered = false;
+  bool mainExamEntered = false;
+//while loop to iterate through the following output
   while (true) {
     print("""====Welcome $name admission no $admn_no to Gumbaru gradechecker====
                  1.Check Cat 1 Results
@@ -55,38 +67,40 @@ void main() {
                  3.Check Main exam Results
                  4.Check Average 
                  5.Exit""");
+                  //getting users input
     stdout.write("Enter choice : ");
+    //converting the input from the user to an interger and storing it to a variable choice
     int choice = int.parse(stdin.readLineSync()!);
+    //switch case statement to check whether a specific condition is meet through the choices
     switch (choice) {
       case 1:
         stdout.write("Enter Cat 1 : ");
-        int cat_1 = int.parse(stdin.readLineSync()!);
-        average.cat_1 = cat_1;
-        average.printResults(cat_1);
+        student.cat_1 = int.parse(stdin.readLineSync()!);
+        student.printResults(student.cat_1, "Cat 1");
+        cat1Entered = true;
         break;
       case 2:
         stdout.write("Enter Cat 2 : ");
-        int cat_2 = int.parse(stdin.readLineSync()!);
-        average.cat_2 = cat_2;
-        average.printResults(cat_2);
+        student.cat_2 = int.parse(stdin.readLineSync()!);
+        student.printResults(student.cat_2, "Cat 2");
+        cat2Entered = true;
         break;
       case 3:
         stdout.write("Enter Main exam : ");
-        int main_exam = int.parse(stdin.readLineSync()!);
-        average.main_exam = main_exam;
-        average.printResults(main_exam);
+        student.main_exam = int.parse(stdin.readLineSync()!);
+        student.printResults(student.main_exam, "Main Exam");
+        mainExamEntered = true;
         break;
       case 4:
-        stdout.write("Enter Cat 1 : ");
-        int cat1 = int.parse(stdin.readLineSync()!);
-        stdout.write("Enter Cat 2 : ");
-        int cat2 = int.parse(stdin.readLineSync()!);
-        stdout.write("Enter Main exam : ");
-        int mainExam = int.parse(stdin.readLineSync()!);
-        average.cat_1 = cat1;
-        average.cat_2 = cat2;
-        average.main_exam = mainExam;
-        average.evaluateResults();
+        if (!cat1Entered || !cat2Entered || !mainExamEntered) {
+          print(
+              "Please enter all exam results before calculating the average.");
+        } else {
+          student.printResults(student.cat_1, "Cat 1");
+          student.printResults(student.cat_2, "Cat 2");
+          student.printResults(student.main_exam, "Main Exam");
+          student.evaluateResults();
+        }
         break;
       case 5:
         exit(0);
